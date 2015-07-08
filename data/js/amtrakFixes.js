dad78a8423541d20e796e2159fb05486b37e89e5
Jared Siirila <siirila@gmail.com>
2015-07-07 21:45:54 -0400

    Add an accel-shift-i keyboard shortcut

--- data/js/amtrakFixes.js
+++ data/js/amtrakFixes.js
@@ -5,8 +5,30 @@ console.log("Fixing Amtrak date selecting issues");
 allowEdits(departureEl);
 allowEdits(returnEl);
 
+function allowEdits(d) {
+ element = $('#wdfdate'+d);
+  if(element) {
+  	console.log("DING DING! Matched a Date with element:",element);
+        element.parent().append('<input class="wdfDate ie6-text" type="date" size="22" id="date'+d+'" maxlength="22" dfield="'+d+'">');
+        element.hide();
+        $('#date'+d).change(setDate);
+        console.log('complete');
+  } else {
+  	console.log("WHY!!! Why didn't an element match");
+  }
+}
 
-function allowEdits(element) {
+function setDate() {
+  console.log('hello there');
+  val = $(this).val();
+  dfield = $(this).attr('dfield');
+  newDate = new Date(val);
+  if(isValidDate(newDate)) {
+  	$('wdfdate'+dfield).val(formatDate(newDate));
+  }
+}
+
+/*function allowEdits(element) {
   if(element) {
   	console.log("DING DING! Matched a Date with element:" + element);
     element.removeAttr("readonly");
@@ -27,7 +49,7 @@ function setDate() {
   if(isValidDate(newDate)) {
   	$(this).val(formatDate(newDate));
   }
-}
+}*/
 
 function isValidDate(d) {
   if ( Object.prototype.toString.call(d) !== "[object Date]" )
